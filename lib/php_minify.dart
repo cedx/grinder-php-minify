de843417d985454b8566bc04e50648c1d2de60cc
Cédric Belin <cedric@belin.io>
2017-05-21 23:22:05 +0200

    Implemented the `phpMinify` function

--- lib/php_minify.dart
+++ lib/php_minify.dart
@@ -10,7 +10,19 @@ part 'src/fast_transformer.dart';
 part 'src/minifier.dart';
 part 'src/safe_transformer.dart';
 
-/// TODO
-Future phpMinify(dynamic src, dynamic dest) async {
-  // TODO
+/// Minifies the PHP scripts located in the specified [source] directory and saves the resulting output to the specified [destination] directory.
+///
+/// The processing can be customized using the following options:
+/// - [binary]: the path to the PHP executable. Defaults to `"php"`.
+/// - [mode]: the transformation type. Defaults to `"safe"`.
+/// - [pattern]: TODO Defaults to `"*.php"`.
+/// - [silent]: a value indicating whether to silent the plug-in output. Defaults to `false`.
+Future phpMinify(dynamic source, dynamic destination, {dynamic binary, String mode = 'safe', String pattern = '*.php', bool silent = false}) async {
+  var minifier = new Minifier(binary != null ? new FilePath(binary).path : await which('php'))
+    ..mode = mode
+    ..silent = silent;
+
+  var sources = new FileSet.fromDir(new FilePath(source).asDirectory, pattern: pattern, recurse: true);
+  log(sources.files.toString());
+  return minifier.transform(sources, new FilePath(destination).asDirectory);
 }
