43f913ca02b1357abfab967bb719460e464bd805
Cédric Belin <cedric@belin.io>
2017-05-22 00:20:36 +0200

    Implemented the functions

--- lib/php_minify.dart
+++ lib/php_minify.dart
@@ -11,19 +11,40 @@ part 'src/fast_transformer.dart';
 part 'src/minifier.dart';
 part 'src/safe_transformer.dart';
 
-/// Minifies the PHP scripts located in the specified [source] directory and saves the resulting output to the specified [destination] directory.
+/// Minifies the specified PHP [source] directory and saves the resulting output to the specified [destination] directory.
 ///
 /// The processing can be customized using the following options:
-/// - [binary]: the path to the PHP executable. Defaults to `"php"`.
+/// - [binary]: the path to the PHP executable. Defaults to the `php` binary found on the system path.
 /// - [mode]: the transformation type. Defaults to `"safe"`.
-/// - [pattern]: TODO Defaults to `"*.php"`.
+/// - [pattern]: the file pattern used to match the eligible PHP scripts. Defaults to `"*.php"`.
+/// - [recurse]: a value indicating whether to process the directory recursively. Defaults to `true`.
 /// - [silent]: a value indicating whether to silent the plug-in output. Defaults to `false`.
-Future phpMinify(dynamic source, dynamic destination, {dynamic binary, String mode = 'safe', String pattern = '*.php', bool silent = false}) async {
+Future minify(dynamic source, dynamic destination, {dynamic binary, String mode = 'safe', String pattern = '*.php', bool recurse = true, bool silent = false}) async {
   var minifier = new Minifier(binary != null ? new FilePath(binary).path : await which('php'))
     ..mode = mode
     ..silent = silent;
 
-  var sources = new FileSet.fromDir(new FilePath(source).asDirectory, pattern: pattern, recurse: true);
-  log(sources.files.toString());
-  return minifier.processDirectory(sources, new FilePath(destination).asDirectory);
+  return minifier.processDirectory(
+    new FilePath(source).asDirectory,
+    new FilePath(destination).asDirectory,
+    pattern: pattern,
+    recurse: recurse
+  );
+}
+
+/// Minifies the specified PHP [source] file and saves the resulting output to the specified [destination] file.
+///
+/// The processing can be customized using the following options:
+/// - [binary]: the path to the PHP executable. Defaults to the `php` binary found on the system path.
+/// - [mode]: the transformation type. Defaults to `"safe"`.
+/// - [silent]: a value indicating whether to silent the plug-in output. Defaults to `false`.
+Future minifyFile(dynamic source, dynamic destination, {dynamic binary, String mode = 'safe', bool silent = false}) async {
+  var minifier = new Minifier(binary != null ? new FilePath(binary).path : await which('php'))
+    ..mode = mode
+    ..silent = silent;
+
+  return minifier.processFile(
+    new FilePath(source).asFile,
+    new FilePath(destination).asFile
+  );
 }
