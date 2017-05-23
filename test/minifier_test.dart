372688cb86a462a33bbcd0d812b659c1e3f3b1fe
Cédric Belin <cedric@belin.io>
2017-05-23 20:05:22 +0200

    Fixed the regressions

--- test/minifier_test.dart
+++ test/minifier_test.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+import 'package:grinder/grinder.dart';
 import 'package:grinder_php_minify/php_minify.dart';
 import 'package:test/test.dart';
 
@@ -7,13 +7,13 @@ void main() => group('Minifier', () {
   group('.mode', () {
     test('should be `safe` if the underlying transformer is a `SafeTransformer` one', () {
       var minifier = new Minifier();
-      minifier.transform = new SafeTransformer(minifier);
+      minifier.transformer = new SafeTransformer(minifier);
       expect(minifier.mode, equals('safe'));
     });
 
     test('should be `fast` if the underlying transformer is a `FastTransformer` one', () {
       var minifier = new Minifier();
-      minifier.transform = new FastTransformer(minifier);
+      minifier.transformer = new FastTransformer(minifier);
       expect(minifier.mode, equals('fast'));
     });
 
@@ -21,34 +21,34 @@ void main() => group('Minifier', () {
       var minifier = new Minifier();
 
       minifier.mode = 'fast';
-      expect(minifier.transform, new isInstanceOf<FastTransformer>());
+      expect(minifier.transformer, new isInstanceOf<FastTransformer>());
 
       minifier.mode = 'safe';
-      expect(minifier.transform, new isInstanceOf<SafeTransformer>());
+      expect(minifier.transformer, new isInstanceOf<SafeTransformer>());
     });
   });
 
-  group('.transform', () {
-    var script = new File('test/fixtures/sample.php');
+  group('.transformer', () {
+    var script = getFile('test/fixtures/sample.php');
     var minifier = new Minifier()..silent = true;
 
     test('should remove the inline comments', () async {
-      expect(await minifier.transform(script), contains("<?= 'Hello World!' ?>"));
+      expect(await minifier.transformer.transform(script), contains("<?= 'Hello World!' ?>"));
     });
 
     test('should remove the multi-line comments', () async {
-      expect(await minifier.transform(script), contains('namespace dummy; class Dummy'));
+      expect(await minifier.transformer.transform(script), contains('namespace dummy; class Dummy'));
     });
 
     test('should remove the single-line comments', () async {
-      expect(await minifier.transform(script), contains(r'$className = get_class($this); return $className;'));
+      expect(await minifier.transformer.transform(script), contains(r'$className = get_class($this); return $className;'));
     });
 
     test('should remove the whitespace', () async {
-      expect(await minifier.transform(script), contains('__construct() { }'));
+      expect(await minifier.transformer.transform(script), contains('__construct() { }'));
     });
 
-    var transformer = minifier.transform as FastTransformer;
+    var transformer = minifier.transformer as FastTransformer;
     if (transformer != null) transformer.close();
   });
 });
