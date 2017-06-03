5d6115bf2112d5eb2c34ebb00efe10a01373ee58
Cédric Belin <cedric@belin.io>
2017-06-04 00:32:51 +0200

    Updated the documentation

--- lib/grinder_php_minify.dart
+++ lib/grinder_php_minify.dart
@@ -18,10 +18,10 @@ part 'src/transformer.dart';
 ///
 /// The processing can be customized using the following options:
 /// - [binary]: the path to the PHP executable. Defaults to the `php` binary found on the system path.
-/// - [mode]: the transformation type. Defaults to `"safe"`.
-/// - [pattern]: the file pattern used to match the eligible PHP scripts. Defaults to `"*.php"`.
-/// - [recurse]: a value indicating whether to process the directory recursively. Defaults to `true`.
-/// - [silent]: a value indicating whether to silent the plug-in output. Defaults to `false`.
+/// - [mode]: the transformation type.
+/// - [pattern]: the file pattern used to match the eligible PHP scripts.
+/// - [recurse]: a value indicating whether to process the directory recursively.
+/// - [silent]: a value indicating whether to silent the plug-in output.
 Future compress(dynamic source, dynamic destination, {dynamic binary, String mode = 'safe', String pattern = '*.php', bool recurse = true, bool silent = false}) async {
   var minifier = new Minifier(binary != null ? new FilePath(binary).path : await which('php'))
     ..mode = mode
@@ -39,8 +39,8 @@ Future compress(dynamic source, dynamic destination, {dynamic binary, String mod
 ///
 /// The processing can be customized using the following options:
 /// - [binary]: the path to the PHP executable. Defaults to the `php` binary found on the system path.
-/// - [mode]: the transformation type. Defaults to `"safe"`.
-/// - [silent]: a value indicating whether to silent the plug-in output. Defaults to `false`.
+/// - [mode]: the transformation type.
+/// - [silent]: a value indicating whether to silent the plug-in output.
 Future compressFile(dynamic source, dynamic destination, {dynamic binary, String mode = 'safe', bool silent = false}) async {
   var minifier = new Minifier(binary != null ? new FilePath(binary).path : await which('php'))
     ..mode = mode
