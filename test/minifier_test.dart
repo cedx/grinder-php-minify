8aaa1caa1c43994b411193e213ab38210164b0d4
Cédric Belin <cedric@belin.io>
2017-10-05 14:43:31 +0200

    Removed a useless unit test

--- test/minifier_test.dart
+++ test/minifier_test.dart
@@ -4,28 +4,6 @@ import 'package:test/test.dart';
 
 /// Tests the features of the [Minifier] class.
 void main() => group('Minifier', () {
-  group('.transformer', () {
-    var script = getFile('test/fixtures/sample.php');
-    var minifier = new Minifier(silent: true);
-    tearDownAll(() => minifier.transformer.close());
-
-    test('should remove the inline comments', () async {
-      expect(await minifier.transformer.transform(script), contains("<?= 'Hello World!' ?>"));
-    });
-
-    test('should remove the multi-line comments', () async {
-      expect(await minifier.transformer.transform(script), contains('namespace dummy; class Dummy'));
-    });
-
-    test('should remove the single-line comments', () async {
-      expect(await minifier.transformer.transform(script), contains(r'$className = get_class($this); return $className;'));
-    });
-
-    test('should remove the whitespace', () async {
-      expect(await minifier.transformer.transform(script), contains('__construct() { }'));
-    });
-  });
-
   group('.processDirectory()', () {
     var minifier = new Minifier(silent: true);
     var testDir = getDir('var/test/processDirectory');
