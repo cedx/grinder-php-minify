e3e9f26287fe8e0b434c8bf32f9ec6a48a3a99d6
Cédric Belin <cedric@belin.io>
2017-10-05 15:29:27 +0200

    Updated a build task

--- tool/grind.dart
+++ tool/grind.dart
@@ -34,6 +34,8 @@ void lint() => Analyzer.analyze(_sources);
 /// Runs all the test suites.
 @Task('Run the tests')
 Future test() async {
+  delete(getDir('var/test'));
+
   await Future.wait([
     Dart.runAsync('test/all.dart', vmArgs: const ['--checked', '--enable-vm-service', '--pause-isolates-on-exit']),
     Pub.runAsync('coverage', script: 'collect_coverage', arguments: const ['--out=var/coverage.json', '--resume-isolates', '--wait-paused'])
