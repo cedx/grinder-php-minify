ac89c44aa33a44753e5e9dcf1d3779288b6602a0
Cédric Belin <cedric@belin.io>
2017-11-06 20:19:48 +0100

    Updated the unit tests

--- test/functions_test.dart
+++ test/functions_test.dart
@@ -9,7 +9,7 @@ void main() {
     var output = joinFile(testDir, const ['sample.php']);
 
     test('should remove the comments and whitespace from the scripts of a directory', () async {
-      await compressDirectory('test/fixtures', testDir.path, silent: true);
+      await compressDirectory('test/fixtures', testDir.path, mode: 'fast', silent: true);
       expect(await output.readAsString(), allOf(
         contains("<?= 'Hello World!' ?>"),
         contains('namespace dummy; class Dummy'),
@@ -24,7 +24,7 @@ void main() {
     var output = joinFile(testDir, const ['sample.php']);
 
     test('should remove the comments and whitespace from a file', () async {
-      await compressFile('test/fixtures/sample.php', output.path, silent: true);
+      await compressFile('test/fixtures/sample.php', output.path, mode: 'safe', silent: true);
       expect(await output.readAsString(), allOf(
         contains("<?= 'Hello World!' ?>"),
         contains('namespace dummy; class Dummy'),
