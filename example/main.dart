da5349ff18dc69adc2a98635f865e438aa833252
Cédric Belin <cedric@belin.io>
2018-07-08 01:22:08 +0200

    Added an example code

---
+++ example/main.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'package:grinder/grinder.dart';
+import 'package:grinder_php_minify/grinder_php_minify.dart' as php_minify;
+
+@Task('Compresses the PHP scripts from a given directory')
+Future<void> compressPhpDirectory() => php_minify.compressDirectory('path/to/src', 'path/to/out');
+
+@Task('Compresses a given PHP script')
+Future<void> compressPhpFile() => php_minify.compressFile('path/to/src.php', 'path/to/out.php');
+
+@Task('Compresses a given set of PHP scripts')
+Future<void> compressPhpFiles() {
+  var sourceDir = getDir('path/to/src');
+  var fileSet = FileSet.fromDir(sourceDir, pattern: '*.php', recurse: true);
+  return php_minify.compressFiles(fileSet.files, 'path/to/out', base: sourceDir.path);
+}
