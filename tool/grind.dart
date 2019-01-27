9aafeca502e193798d793d5ef7865a030db80c7b
Cédric Belin <cedric@belin.io>
2019-01-27 20:01:10 +0100

    Added a "serve" build task

--- tool/grind.dart
+++ tool/grind.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:grinder/grinder.dart';
 
 /// Starts the build system.
@@ -26,6 +27,12 @@ void fix() => DartFmt.format(existingSourceDirs);
 
 @Task('Performs the static analysis of source code')
 void lint() => Analyzer.analyze(existingSourceDirs);
+
+@Task('Starts the development server.')
+Future<void> serve() {
+  log('Serving `lib/php` on http://localhost:8000');
+  return Process.start('php', ['-S', 'localhost:8000', '-t', 'lib/php'], mode: ProcessStartMode.inheritStdio);
+}
 
 @DefaultTask('Runs the test suites')
 Future test() async {
