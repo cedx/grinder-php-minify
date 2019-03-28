5f770c02e2b898cbf38497975a71a6b63682c70c
Cédric Belin <cedric@belin.io>
2019-03-28 10:39:36 +0100

    Fixed a typo

--- tool/grind.dart
+++ tool/grind.dart
@@ -28,7 +28,7 @@ void fix() => DartFmt.format(existingSourceDirs);
 @Task('Performs the static analysis of source code')
 void lint() => Analyzer.analyze(existingSourceDirs);
 
-@Task('Starts the development server.')
+@Task('Starts the development server')
 Future<void> serve() {
   log('Serving "lib/php" on http://localhost:8000');
   return Process.start('php', ['-S', '127.0.0.1:8000', '-t', 'lib/php'], mode: ProcessStartMode.inheritStdio);
