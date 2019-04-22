5229cf27b4464f9bf8e443e2b20b0406a277fca5
Cédric Belin <cedric@belin.io>
2019-04-22 22:38:14 +0200

    Updated the coverage collection

--- tool/grind.dart
+++ tool/grind.dart
@@ -1,8 +1,7 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
 import 'package:grinder/grinder.dart';
-import 'package:pedantic/pedantic.dart';
+import 'package:grinder_coveralls/grinder_coveralls.dart';
 
 /// Starts the build system.
 Future main(List<String> args) => grind(args);
@@ -15,7 +14,7 @@ void clean() {
 }
 
 @Task('Uploads the results of the code coverage')
-void coverage() => Pub.run('coveralls', arguments: ['var/lcov.info']);
+Future<void> coverage() async => uploadCoverage(await getFile('var/lcov.info').readAsString());
 
 @Task('Builds the documentation')
 Future<void> doc() async {
@@ -33,27 +32,12 @@ void lint() => Analyzer.analyze(existingSourceDirs);
 
 @Task('Starts the development server')
 Future<void> serve() {
-  log('Serving "lib/php" on http://localhost:8000');
-  return Process.start('php', ['-S', '127.0.0.1:8000', '-t', 'lib/php'], mode: ProcessStartMode.inheritStdio);
+  log('Serving "${libDir.path}/php" on http://localhost:8000');
+  return Process.start('php', ['-S', '127.0.0.1:8000', '-t', '${libDir.path}/php'], mode: ProcessStartMode.inheritStdio);
 }
 
 @Task('Runs the test suites')
-Future<void> test() async {
-  await Pub.runAsync('coverage', script: 'collect_coverage', arguments: [
-    '--out=var/coverage.json',
-    '--resume-isolates',
-    '--uri=${await _profileTest(getFile('test/all.dart'))}',
-    '--wait-paused'
-  ]);
-
-  return Pub.runAsync('coverage', script: 'format_coverage', arguments: [
-    '--in=var/coverage.json',
-    '--lcov',
-    '--out=var/lcov.info',
-    '--packages=.packages',
-    '--report-on=${libDir.path}'
-  ]);
-}
+Future<void> test() => collectCoverage(getDir('test'), reportOn: [libDir.path], saveAs: 'var/lcov.info');
 
 @Task('Upgrades the project to the latest revision')
 void upgrade() {
@@ -65,24 +49,3 @@ void upgrade() {
 
 @Task('Watches for file changes')
 void watch() => Pub.run('build_runner', arguments: ['watch', '--delete-conflicting-outputs']);
-
-/// Profiles the execution of the specified test file.
-/// Returns the URI that Observatory is listening on.
-Future<Uri> _profileTest(File testFile) async {
-  var counter = 0;
-  final completer = Completer<Uri>();
-
-  final process = await Process.start('dart', ['--enable-vm-service', '--pause-isolates-on-exit', testFile.path]);
-  process.stderr.transform(utf8.decoder).listen((data) => print(data.trimRight()));
-  process.stdout.transform(utf8.decoder).listen((data) {
-    print(data.trimRight());
-    if (++counter == 1) {
-      final match = RegExp(r'^Observatory listening on (.*)').firstMatch(data);
-      final uri = match != null ? match[1].trim() : 'http://127.0.0.1:8181/';
-      completer.complete(Uri.parse(uri));
-    }
-  });
-
-  unawaited(process.exitCode.then((code) => exitCode = code));
-  return completer.future;
-}
