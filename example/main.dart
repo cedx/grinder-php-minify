5741f0e3952b7afcd503c31214769f90f437cf8a
Cédric Belin <cedric@belin.io>
2019-04-24 19:58:15 +0200

    Updated the example code

--- example/main.dart
+++ example/main.dart
@@ -2,7 +2,7 @@ import 'package:grinder/grinder.dart';
 import 'package:grinder_php_minify/grinder_php_minify.dart' as php_minify;
 
 @Task('Compresses the PHP scripts from a given directory')
-Future<void> compressPhpDirectory() => php_minify.compress(getDir('path/to/src'), getDir('path/to/out'));
+Future<void> compressDirectory() => php_minify.compress(getDir('path/to/src'), getDir('path/to/out'));
 
 @Task('Compresses a given PHP script')
-Future<void> compressPhpFile() => php_minify.compress(getFile('path/to/src.php'), getFile('path/to/out.php'));
+Future<void> compressFile() => php_minify.compress(getFile('path/to/src.php'), getFile('path/to/out.php'));
