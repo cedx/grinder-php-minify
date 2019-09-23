d136963181c2a884ea1e3b9ea5de5d2bab01675d
Cédric Belin <cedric@belin.io>
2019-09-24 00:00:10 +0200

    Updated the build system

--- tool/grind.dart
+++ tool/grind.dart
@@ -32,7 +32,7 @@ void lint() => Analyzer.analyze(existingSourceDirs);
 
 @Task('Publishes the package to the registry')
 @Depends(clean, fix)
-void publish() => run('pub', arguments: ['publish', '--force']);
+void publish() => run('pub', arguments: ['publish', '--force'], runOptions: RunOptions(runInShell: true));
 
 @Task('Starts the development server')
 Future<void> serve() {
