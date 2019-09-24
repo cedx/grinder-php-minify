2f7ea9eda31883d019b794d79ae730bc03fe7b6e
Cédric Belin <cedric@belin.io>
2019-09-24 20:54:03 +0200

    Using `glob` patterns instead of `FileSystemEntity` parameters for providing the list of files to be proceeded

--- lib/grinder_php_minify.dart
+++ lib/grinder_php_minify.dart
@@ -14,28 +14,19 @@ part 'src/minifier.dart';
 part 'src/safe_transformer.dart';
 part 'src/transformer.dart';
 
-/// Minifies the specified [source] file or directory containing PHP files,
-/// and writes the resulting output to the specified [destination].
+/// Minifies the PHP scripts corresponding to the specified file [patterns],
+/// and writes the resulting output to the specified [destination] directory.
 ///
-/// If [source] is a directory, it will be scanned according to the value of the [recurse] parameter for files matching the specified [pattern].
 /// If [base] is provided, this path is removed from the target path of the destination files.
 /// If [binary] is provided, it will be used as the path to the PHP executable.
 /// The [mode] value indicates the operation mode of the minifier, while the [silent] value indicates whether to silent the minifier output.
-Future<void> compress(FileSystemEntity source, FileSystemEntity destination, {
+Future<void> phpMinify(patterns, destination, {
   String base,
   String binary,
   TransformMode mode = TransformMode.safe,
-  String pattern = '*.php',
-  bool recurse = true,
   bool silent = false
 }) async {
-  final minifier = Minifier(binary: binary ?? await where('php', onError: (command) => command), mode: mode, silent: silent);
-  if (source is! Directory) {
-    final output = destination is Directory ? joinDir(destination, [baseName(source)]) : destination;
-    return minifier.compressFile(source, output);
-  }
-
-  final output = destination is Directory ? destination : destination.parent;
-  final fileSet = FileSet.fromDir(source, pattern: pattern, recurse: recurse);
-  return minifier.compressFiles(fileSet.files, output, base: base ?? source.path);
+  final globs = patterns is List ? patterns : [patterns];
+  return Minifier(binary: binary ?? await where('php', onError: (command) => command), mode: mode, silent: silent)
+    .run(globs.map((pattern) => Glob(pattern)), FilePath(destination).asDirectory, base: base);
 }
