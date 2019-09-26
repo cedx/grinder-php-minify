fb0e8b2ba0500e4c9f8efc0b05780a4edaa863d3
Cédric Belin <cedric@belin.io>
2019-09-26 15:01:03 +0200

    Updated the tests

--- test/factory_test.dart
+++ test/factory_test.dart
@@ -1,22 +1,10 @@
-import 'dart:io';
 import 'package:grinder/grinder.dart';
 import 'package:grinder_php_minify/grinder_php_minify.dart';
 import 'package:test/test.dart';
 
 /// Tests the features of the [phpMinify] function.
 void main() => group('phpMinify()', () {
-  test('should remove the comments and whitespace from the PHP scripts', () async {
-    const testDir = 'var/test/phpMinify';
-    await phpMinify('test/**.php', testDir, base: 'test/fixtures', silent: true);
-    expect(await joinFile(getDir(testDir), ['sample.php']).readAsString(), allOf(
-      contains("<?= 'Hello World!' ?>"),
-      contains('namespace dummy; class Dummy'),
-      contains(r'$className = get_class($this); return $className;'),
-      contains('__construct() { }')
-    ));
-  });
-
-  test('should support the fast transformer', () async {
+  test('should remove the comments and whitespace using the fast transformer', () async {
     const testDir = 'var/test/phpMinify.fast';
     await phpMinify('test/**.php', testDir, base: 'test/fixtures', mode: TransformMode.fast, silent: true);
     expect(await joinFile(getDir(testDir), ['sample.php']).readAsString(), allOf(
@@ -27,7 +15,7 @@ void main() => group('phpMinify()', () {
     ));
   });
 
-  test('should support the safe transformer', () async {
+  test('should remove the comments and whitespace using the safe transformer', () async {
     const testDir = 'var/test/phpMinify.safe';
     await phpMinify('test/**.php', testDir, base: 'test/fixtures', mode: TransformMode.safe, silent: true);
     expect(await joinFile(getDir(testDir), ['sample.php']).readAsString(), allOf(
