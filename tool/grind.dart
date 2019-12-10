dfcae7927f566d78e39cdb630c61abc5f45fb8f5
Cédric Belin <cedric@belin.io>
2019-12-10 15:58:54 +0100

    Updated the build system

--- tool/grind.dart
+++ tool/grind.dart
@@ -10,7 +10,7 @@ Future main(List<String> args) => grind(args);
 void clean() {
   defaultClean();
   ['.dart_tool', 'doc/api', 'var/test', webDir.path].map(getDir).forEach(delete);
-  FileSet.fromDir(getDir('var'), pattern: '*.{info,json}', recurse: true).files.forEach(delete);
+  FileSet.fromDir(getDir('var'), pattern: '!.*', recurse: true).files.forEach(delete);
 }
 
 @Task('Uploads the results of the code coverage')
